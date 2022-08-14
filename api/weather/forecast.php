04c1b6f1b925113fff40dffe93641a22eadeef2c
Ho Yin (Vis) Wong <viswongdev@gmail.com>
2022-08-14 22:41:12 +0100

    disable error output

--- api/weather/forecast.php
+++ api/weather/forecast.php
@@ -6,9 +6,9 @@
   include_once '../../config/Database.php';
   include_once '../../models/Weather.php';
 
-  ini_set('display_errors', '1');
-  ini_set('display_startup_errors', '1');
-  error_reporting(E_ALL);
+  // ini_set('display_errors', '1');
+  // ini_set('display_startup_errors', '1');
+  // error_reporting(E_ALL);
 
   // Instantiate DB & connect
   $database = new Database();
